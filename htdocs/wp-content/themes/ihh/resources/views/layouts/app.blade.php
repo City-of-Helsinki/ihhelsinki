80900c96a7d142d02b6e2a7afeae881cfa8dfd8f
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2020-08-17 14:38:09 +0300

    IHHW-008 use main tag instead of <div role="document">

--- htdocs/wp-content/themes/ihh/resources/views/layouts/app.blade.php
+++ htdocs/wp-content/themes/ihh/resources/views/layouts/app.blade.php
@@ -5,11 +5,9 @@
     @php do_action('get_header') @endphp
     @include('components.navigation')
     @include('partials.header')
-    <div class="wrap" role="document">
-      <main class="main">
-        @yield('content')
-      </main>
-    </div>
+    <main class="wrap main">
+      @yield('content')
+    </main>
     @php do_action('get_footer') @endphp
     @include('partials.footer')
     @php wp_footer() @endphp
