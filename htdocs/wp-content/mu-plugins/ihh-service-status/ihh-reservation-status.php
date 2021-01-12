bfd81999caf32af3fda5769be272082e19ed63d6
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-01-12 10:42:52 +0200

    Fetch api data, handle caching

--- htdocs/wp-content/mu-plugins/ihh-service-status/ihh-reservation-status.php
+++ htdocs/wp-content/mu-plugins/ihh-service-status/ihh-reservation-status.php
@@ -13,45 +13,85 @@ namespace IHH;
 
 require_once( dirname( __FILE__ ) . '/hooks.php' );
 
+// Cache key for service statuses.
+CONST IHH_SERVICE_CACHE_KEY = 'service_status';
+
+// Cache key for service status timestamp.
+CONST IHH_SERVICE_CACHE_TIMESTAMP_KEY = 'ihh_last_status_cache';
+
+// Cache valid for seconds.
+CONST IHH_SERVICE_CACHE_EXPIRATION = 300;
+
+// Status api url.
+CONST IHH_SERVICE_API_URL = 'https://lt36853.loomis.fi:9090/MobileTicket/branches/1/services/wait-info/';
+
+// Status api key in wp_options table.
+CONST IHH_SERVICE_API_TOKEN_WP_OPTION_KEY = 'service_api_token';
+
+// Add wp-cron to fetch the data every 6 minutes
+function ihh_cron_schedules($schedules){
+  if(!isset($schedules["five_minutes"])){
+    $schedules["six_minutes"] = array(
+      'interval' => 360,
+      'display' => __('Once every 5 minutes'));
+  }
+  return $schedules;
+}
+
+add_filter('cron_schedules', 'IHH\ihh_cron_schedules');
+wp_schedule_event(time(), 'six_minutes', 'my_schedule_hook', 'fetch_service_status');
+
+// Fetch the data from api.
 function fetch_service_status() {
-  #https://lt36853.loomis.fi:9090/managementinformation/v2/branches/1/queues/
-  $CACHE_KEY = 'service_status';
-  $CACHE_EXPIRATION = 120;
-
-  $API_URL = 'https://lt36853.loomis.fi:9090';
-  $ENDPOINT = '/MobileTicket/branches/1/services/wait-info/';
-
-  if($data = get_transient($CACHE_KEY)){
-    return $data;
+  // Return cached if possible
+  if ($data = get_transient(IHH_SERVICE_CACHE_KEY)) {
+    $json = json_decode($data);
+    $json['timestamp'] = get_transient(IHH_SERVICE_CACHE_TIMESTAMP_KEY);
+    return $json;
   }
 
-  if(!$TOKEN = get_option('service_api_token')){
-    // service api's security token has not been set to options.
+  // Api security token should be set to wp_options.
+  if (!$token = get_option(IHH_SERVICE_API_TOKEN_WP_OPTION_KEY)) {
     return false;
   }
 
-  $token = 'no-tokens-in-git';
+  $response = fetch_remote_data($token);
+
+  if (!request_ok($response)) {
+    return false;
+  }
+
+  $time = time();
+  handle_cache($response, $time);
+  return handle_response_data($response, $time);
+}
+
+// Actual GET request.
+function fetch_remote_data($token){
   $args = [
     'headers' => [
       'auth-token' => $token
     ]
   ];
+  return wp_remote_get(IHH_SERVICE_API_URL, $args);
+}
 
-  $data = wp_remote_get('https://lt36853.loomis.fi:9090/MobileTicket/branches/1/services/wait-info/', $args);
+// Is request valid.
+function request_ok($request) {
+  return !is_wp_error($request) && isset($request['response']) && $request['response']['code'] === 200;
+}
 
-  if(!request_ok($data)) {
-    #var_dump($data);die();
-    return false;
-  }
-  #var_dump($data['body']);die();
+// Cache fetched data.
+function handle_cache($response, $time){
+  set_transient(IHH_SERVICE_CACHE_TIMESTAMP_KEY, $time);
+  set_transient(IHH_SERVICE_CACHE_KEY, $response, IHH_SERVICE_CACHE_EXPIRATION);
+}
 
-  set_transient($CACHE_KEY, json_encode($data), $CACHE_EXPIRATION);
-
-  #die('no');
+// Return response
+function handle_response_data($response, $time) {
+  $body = $response['http_response'];
+  $json = $body->get_data();
+  $data = json_decode($json);
+  $data['timestamp'] = $time;
   return $data;
 }
-
-function request_ok($request){
-  return !is_wp_error($request) && isset($request['code']) && $request['code'] == 200;
-}
-
