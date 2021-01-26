2331a969557bd646d8c5bfd469a1505c61d58e8f
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-01-26 08:52:39 +0200

    service list update

--- htdocs/wp-content/themes/ihh/resources/views/components/home/services.blade.php
+++ htdocs/wp-content/themes/ihh/resources/views/components/home/services.blade.php
@@ -6,34 +6,39 @@
 
 @if(have_rows('services'))
   @while(have_rows('services')) @php the_row() @endphp
-
-  <section class="services">
+  <section class="services" style="margin-bottom: 0; padding: 0;">
     <div class="container">
       <div class="services-header">
         <img src="{{ the_sub_field('image') }}" alt="">
       </div>
-      <div class="services-content">
-        <h2>{{the_sub_field('title')}}</h2>
-        <ul class="home-services-list">
-          @while($services->have_posts())
-            @php $services->the_post();
+      <h2 style="margin-top: 80px; font-size: 55px;">{{the_sub_field('title')}}</h2>
+    </div>
+  </section>
+  <section class="services" style="padding-top: 0;">
+    <div class="services-background">
+      <div class="container">
+        <div class="services-content">
+          <ul class="home-services-list">
+            @while($services->have_posts())
+              @php $services->the_post();
             $logoUrl = '';
             $logoAlt = '';
             if($logoArray = get_field('logo', get_the_ID())){
               $logoUrl = $logoArray['url'];
               $logoAlt = $logoArray['alt'];
             }
-            @endphp
-          <li class="servicelist-item">
-            <a href="{{ the_permalink() }}#{{ sanitize_title(get_the_title()) }}"
-               @if(get_sub_field('color')) style="background-color: {{get_sub_field('color')}}" @endif>
-              <div><p>{{ the_title() }}</p></div>
-              @if($logoUrl) <div class="image-wrap"><img style="max-width:25%; display:block; margin: 0 auto;" src="{{ $logoUrl }}" alt="{{ $logoAlt }}"/></div> @endif
-              <span class="read-more">{!! pll__('Read more') !!}</span>
-            </a>
-          </li>
-          @endwhile @php wp_reset_postdata() @endphp
-        </ul>
+              @endphp
+              <li class="servicelist-item">
+                <a href="{{ the_permalink() }}#{{ sanitize_title(get_the_title()) }}"
+                   @if(get_sub_field('color')) style="background-color: {{get_sub_field('color')}}" @endif>
+                  <div class="title"><p>{{ the_title() }}</p></div>
+                  @if($logoUrl) <div class="image-wrap"><img src="{{ $logoUrl }}" alt="{{ $logoAlt }}"/></div> @endif
+                  <span class="read-more">{!! pll__('Read more') !!}</span>
+                </a>
+              </li>
+            @endwhile @php wp_reset_postdata() @endphp
+          </ul>
+        </div>
       </div>
     </div>
   </section>
